d6b60e4b959948b8d8f66818af7069a2c72d178b
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-11-04 13:51:13 +1100

    added middleware

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const path = require('path');
+const { clog } = require('./middleware/clog');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // Wildcard route to direct users to index.html
